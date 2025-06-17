e384de47691dc484f2ae91ca5dd7e0b383532219
Lery Ley <leryley8@gmail.com>
2025-06-17 12:53:31 +0700

    update controller

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,11 +1,17 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import productModel from "../models/product.model";
+import { sendSuccessResponse } from "../utils/response";
 
 class ProductController {
     async getAll(res: FastifyRequest, rp: FastifyReply) {
         try {
             const products = await productModel.getAll();
-            return rp.status(200).send(products);
+            if (products[0].length === 0) {
+                return rp.status(404).send({ message: "No products found" });
+            }
+            return sendSuccessResponse(rp, true, "Products fetched successfully", {
+                rows: products[0],
+            });
         } catch (error) {
             console.error("Error fetching products:", error);
             return rp.status(500).send({ error: "Internal Server Error" });
